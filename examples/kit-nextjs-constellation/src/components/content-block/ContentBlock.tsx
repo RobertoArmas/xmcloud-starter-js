638d760390a0b4bbc433492d97ea138e62c0a603
Roberto Armas <roberto.armas@verndale.com>
2025-10-01 08:23:35 -0500

    Constellation Starter Kit

---
+++ examples/kit-nextjs-constellation/src/components/content-block/ContentBlock.tsx
@@ -0,0 +1,42 @@
+import {
+  Text,
+  RichText,
+  Field,
+  withDatasourceCheck,
+  GetComponentServerProps,
+} from '@sitecore-content-sdk/nextjs';
+import { ComponentProps } from 'lib/component-props';
+import { JSX } from 'react';
+
+type ContentBlockProps = ComponentProps & {
+  fields: {
+    heading: Field<string>;
+    content: Field<string>;
+  };
+};
+
+/**
+ * A simple Content Block component, with a heading and rich text block.
+ * This is the most basic building block of a content site, and the most basic
+ * Content SDK component that's useful.
+ */
+const ContentBlock = ({ fields }: ContentBlockProps): JSX.Element => {
+  return (
+    <div className="contentBlock">
+      <Text tag="h2" className="contentTitle" field={fields.heading} />
+
+      <RichText className="contentDescription" field={fields.content} />
+    </div>
+  );
+};
+
+export const getComponentServerProps: GetComponentServerProps = async (
+  rendering,
+  layoutData,
+  context
+) => {
+  return {
+    test: 'hello',
+  };
+};
+export default withDatasourceCheck()<ContentBlockProps>(ContentBlock);
