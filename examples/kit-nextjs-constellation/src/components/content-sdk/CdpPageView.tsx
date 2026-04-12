2954a57d5236b0ba3bd128b7def00453d6b9ea3c
Roberto Armas <roberto.armas@verndale.com>
2026-04-12 09:44:22 -0500

    App router and 2.0 Fresh Start

--- examples/kit-nextjs-constellation/src/components/content-sdk/CdpPageView.tsx
+++ examples/kit-nextjs-constellation/src/components/content-sdk/CdpPageView.tsx
@@ -1,8 +1,8 @@
+'use client';
+import { useEffect, JSX } from 'react';
 import { CdpHelper, useSitecore } from '@sitecore-content-sdk/nextjs';
-import { useEffect } from 'react';
-import { pageView } from '@sitecore-cloudsdk/events/browser';
+import { pageView } from '@sitecore-content-sdk/events';
 import config from 'sitecore.config';
-import { JSX } from 'react';
 
 /**
  * This is the CDP page view component.
